8ac3da866e13b66942392fc71dd299fba89bc9a9
Eazy_Rydah <fabian.rhoda@gmx.net>
2020-02-02 20:42:37 +0100

    sql-file final

--- carparkapp.sql
+++ carparkapp.sql
@@ -21,6 +21,8 @@ USE `carparkapp` ;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`carpark`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`carpark` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`carpark` (
   `carpark_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
@@ -37,6 +39,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`user_role`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`user_role` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`user_role` (
   `user_role_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
@@ -49,6 +53,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`user`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`user` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`user` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
@@ -61,11 +67,11 @@ CREATE TABLE IF NOT EXISTS `carparkapp`.`user` (
   `is_active` TINYINT NULL DEFAULT '0',
   `user_role_id` INT NOT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  UNIQUE INDEX `id_UNIQUE` (`user_id` ASC) VISIBLE,
-  UNIQUE INDEX `password_reset_hash_UNIQUE` (`password_reset_hash` ASC) VISIBLE,
-  UNIQUE INDEX `activation_hash_UNIQUE` (`activation_hash` ASC) VISIBLE,
-  INDEX `fk_user_user_role1_idx` (`user_role_id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
+  UNIQUE INDEX `id_UNIQUE` (`user_id` ASC) ,
+  UNIQUE INDEX `password_reset_hash_UNIQUE` (`password_reset_hash` ASC) ,
+  UNIQUE INDEX `activation_hash_UNIQUE` (`activation_hash` ASC) ,
+  INDEX `fk_user_user_role1_idx` (`user_role_id` ASC) ,
   CONSTRAINT `fk_user_user_role1`
     FOREIGN KEY (`user_role_id`)
     REFERENCES `carparkapp`.`user_role` (`user_role_id`)
@@ -79,6 +85,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`contract`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`contract` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`contract` (
   `contract_id` INT NOT NULL,
   `rfid_id` INT NOT NULL,
@@ -88,9 +96,9 @@ CREATE TABLE IF NOT EXISTS `carparkapp`.`contract` (
   `user_id` INT NOT NULL,
   `carpark_id` INT NOT NULL,
   PRIMARY KEY (`contract_id`),
-  UNIQUE INDEX `id_UNIQUE` (`contract_id` ASC) VISIBLE,
-  INDEX `fk_contract_user1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_contract_carpark1_idx` (`carpark_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`contract_id` ASC) ,
+  INDEX `fk_contract_user1_idx` (`user_id` ASC) ,
+  INDEX `fk_contract_carpark1_idx` (`carpark_id` ASC) ,
   CONSTRAINT `fk_contract_carpark1`
     FOREIGN KEY (`carpark_id`)
     REFERENCES `carparkapp`.`carpark` (`carpark_id`)
@@ -108,6 +116,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`contract_request`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`contract_request` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`contract_request` (
   `contract_request_id` INT NOT NULL AUTO_INCREMENT,
   `contract_auth` INT NOT NULL,
@@ -116,10 +126,10 @@ CREATE TABLE IF NOT EXISTS `carparkapp`.`contract_request` (
   `user_id` INT NOT NULL,
   `contract_id` INT NULL DEFAULT NULL,
   PRIMARY KEY (`contract_request_id`),
-  UNIQUE INDEX `id_UNIQUE` (`contract_request_id` ASC) VISIBLE,
-  INDEX `fk_contract_request_user1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_contract_request_contract1_idx` (`contract_id` ASC) VISIBLE,
-  UNIQUE INDEX `contract_id_UNIQUE` (`contract_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`contract_request_id` ASC) ,
+  UNIQUE INDEX `contract_id_UNIQUE` (`contract_id` ASC) ,
+  INDEX `fk_contract_request_user1_idx` (`user_id` ASC) ,
+  INDEX `fk_contract_request_contract1_idx` (`contract_id` ASC) ,
   CONSTRAINT `fk_contract_request_contract1`
     FOREIGN KEY (`contract_id`)
     REFERENCES `carparkapp`.`contract` (`contract_id`)
@@ -138,19 +148,24 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`csv_report`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`csv_report` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`csv_report` (
   `csv_report_id` INT NOT NULL AUTO_INCREMENT,
   `amount_credit_items` INT NOT NULL,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`csv_report_id`),
-  UNIQUE INDEX `id_UNIQUE` (`csv_report_id` ASC) VISIBLE)
+  UNIQUE INDEX `id_UNIQUE` (`csv_report_id` ASC) )
 ENGINE = InnoDB
+AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `carparkapp`.`share`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`share` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`share` (
   `share_id` INT NOT NULL AUTO_INCREMENT,
   `start_date` DATE NOT NULL,
@@ -159,8 +174,8 @@ CREATE TABLE IF NOT EXISTS `carparkapp`.`share` (
   `is_active` TINYINT NULL DEFAULT NULL,
   `contract_id` INT NOT NULL,
   PRIMARY KEY (`share_id`),
-  UNIQUE INDEX `id_UNIQUE` (`share_id` ASC) VISIBLE,
-  INDEX `fk_share_contract1_idx` (`contract_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`share_id` ASC) ,
+  INDEX `fk_share_contract1_idx` (`contract_id` ASC) ,
   CONSTRAINT `fk_share_contract1`
     FOREIGN KEY (`contract_id`)
     REFERENCES `carparkapp`.`contract` (`contract_id`)
@@ -174,19 +189,21 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`credit_item`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`credit_item` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`credit_item` (
   `credit_item_id` INT NOT NULL AUTO_INCREMENT,
   `share_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `csv_report_id` INT NULL DEFAULT NULL,
   PRIMARY KEY (`credit_item_id`),
-  UNIQUE INDEX `id_UNIQUE` (`credit_item_id` ASC) VISIBLE,
-  INDEX `fk_credit_item_share1_idx` (`share_id` ASC) VISIBLE,
-  INDEX `fk_credit_item_csv_report1_idx` (`csv_report_id` ASC) VISIBLE,
-  UNIQUE INDEX `share_id_UNIQUE` (`share_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`credit_item_id` ASC) ,
+  UNIQUE INDEX `share_id_UNIQUE` (`share_id` ASC) ,
+  INDEX `fk_credit_item_share1_idx` (`share_id` ASC) ,
+  INDEX `fk_credit_item_csv_report1_idx` (`csv_report_id` ASC) ,
   CONSTRAINT `fk_credit_item_csv_report1`
     FOREIGN KEY (`csv_report_id`)
     REFERENCES `carparkapp`.`csv_report` (`csv_report_id`)
-    ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `fk_credit_item_share1`
     FOREIGN KEY (`share_id`)
@@ -201,14 +218,16 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `carparkapp`.`rfid_blockage`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `carparkapp`.`rfid_blockage` ;
+
 CREATE TABLE IF NOT EXISTS `carparkapp`.`rfid_blockage` (
   `rfid_blockage_id` INT NOT NULL AUTO_INCREMENT,
   `is_confirmed` TINYINT NULL DEFAULT '0',
   `share_id` INT NOT NULL,
   PRIMARY KEY (`rfid_blockage_id`),
-  UNIQUE INDEX `id_UNIQUE` (`rfid_blockage_id` ASC) VISIBLE,
-  INDEX `fk_rfid_blockage_share1_idx` (`share_id` ASC) VISIBLE,
-  UNIQUE INDEX `share_id_UNIQUE` (`share_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`rfid_blockage_id` ASC) ,
+  UNIQUE INDEX `share_id_UNIQUE` (`share_id` ASC) ,
+  INDEX `fk_rfid_blockage_share1_idx` (`share_id` ASC) ,
   CONSTRAINT `fk_rfid_blockage_share1`
     FOREIGN KEY (`share_id`)
     REFERENCES `carparkapp`.`share` (`share_id`)
